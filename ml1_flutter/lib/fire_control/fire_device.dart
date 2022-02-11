b4b7871516f87ab0936235f8e79e1ba3e16cf6ae
Maksim Lin <maks@manichord.com>
2022-02-11 20:59:30 +1100

    fix event subscription, add basic screen drawing

--- ml1_flutter/lib/fire_control/fire_device.dart
+++ ml1_flutter/lib/fire_control/fire_device.dart
@@ -4,11 +4,13 @@ import 'package:bonsai/bonsai.dart';
 import 'package:dart_fire_midi/dart_fire_midi.dart' as fire;
 import 'package:flutter_midi_command/flutter_midi_command.dart';
 import 'package:collection/collection.dart';
+import 'package:ml1_flutter/fire_control/oled/screen.dart';
+import 'package:monochrome_draw/monochrome_draw.dart';
 
 class FireDevice {
   final MidiCommand _midi;
   MidiDevice? _device;
-  late final StreamSubscription<MidiPacket>? _rxSubscription;
+  StreamSubscription<MidiPacket>? _rxSubscription;
   final _inputStreamController = StreamController<fire.FireInputEvent>();
 
   Stream get fireEvents => _inputStreamController.stream.asBroadcastStream();
@@ -21,7 +23,7 @@ class FireDevice {
     if (device != null) {
       await _midi.connectToDevice(device);
 
-      _rxSubscription = _midi.onMidiDataReceived?.listen((packet) {
+      _rxSubscription ??= _midi.onMidiDataReceived?.listen((packet) {
         log('received packet: ${packet.data}');
         _inputStreamController.add(fire.FireInputEvent.fromMidi(packet.data));
       });
@@ -30,6 +32,11 @@ class FireDevice {
 
       _device = device;
       log('connected device:$_device');
+
+      final MonoCanvas oledBitmap = MonoCanvas(128, 64);
+      oledBitmap.setCursor(0, 0);
+      oledBitmap.writeString(defaultFont, 2, "Hello :-)", true, true, 1);
+      _midi.sendData(fire.sendBitmap(oledBitmap.data));
     } else {
       Log.e('no Fire device to connect to');
     }
